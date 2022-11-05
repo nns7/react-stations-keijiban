47b9e2297d89af1ac4dabdd34650c8bee6740141
nns7 <nns7snn@gmail.com>
2022-11-05 17:44:02 +0900

    :tada: station2 スレッド一覧機能を作成した。

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [threadsList, setThreadsList] = React.useState(null);
+
+  async function fetchThreadsList() {
+    const response = await fetch(
+      'http://railway-react-bulletin-board.herokuapp.com/threads'
+    );
+    const data = await response.json();
+    return data;
+  }
+
+  React.useEffect(() => {
+    fetchThreadsList().then((data) => {
+      setThreadsList(data);
+    });
+  }, [])
+
+  function reloadThreads() {
+    fetchThreadsList().then((data) => {
+      setThreadsList(data);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>掲示板</p>
+      <p>スレッドを立てる</p>
+      <p>新着スレッド</p>
+      <ul>
+        {threadsList == null ? (
+          <li>更新ボタンを押してください。</li>
+        ) : (
+          threadsList.map((threadItem) => {
+            return (
+              <li>{threadItem.title}</li>
+            );
+          })
+        )}
+      </ul>
+      <p>
+        <button type='button' onClick={reloadThreads}>更新</button>
+      </p>
     </div>
-  );
+  )
 }
 
 export default App;
